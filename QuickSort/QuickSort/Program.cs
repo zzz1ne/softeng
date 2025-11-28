800d8fff18f6a9d87fef96a5b081a8e22589d2ad
Абрамов Валерий <valeraabramov763@gmail.com>
2025-11-28 18:15:22 +0300

    Добавил алгоритм быстрой сортировки Хоара
    Для добавления файлов добавили строки в .gitignore

---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace QuickSort
+{
+    internal class Program
+    {
+        public static void QuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(arr, left, right);
+                QuickSort(arr, left, pivotIndex - 1);
+                QuickSort(arr, pivotIndex + 1, right);
+            }
+        }
+
+        private static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (arr[j] <= pivot)
+                {
+                    i++;
+                    Swap(arr, i, j);
+                }
+            }
+            Swap(arr, i + 1, right);
+            return i + 1;
+        }
+
+        private static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] data = { 5, 1, 4, 2, 8 };
+            QuickSort(data, 0, data.Length - 1);
+        }
+    }
+}
